037e9df777c2f791c71e187947aba802d73da420
txoof <aaron.ciuffo@gmail.com>
2022-05-03 13:54:36 +0200

    add version number, cleanup regex

--- imscc_extract_files.sh
+++ imscc_extract_files.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 TEMPDIR=$(mktemp -d)
 INPUT_FILE=$1
-OUTPUT_PATH=./temp_path/foo
+OUTPUT_PATH=~/Desktop
 MANIFEST_FILE="imsmanifest.xml"
+VERSION="V0.1.1 "
 
 slugify () {
     echo "$1" | iconv -c -t ascii//TRANSLIT | sed -E 's/[~^]+//g' | sed -E 's/[^a-zA-Z0-9]+/_/g' | sed -E 's/^-+|-+$//g' | tr A-Z a-z
@@ -14,8 +15,11 @@ abort() {
     exit
 }
 
+echo $VERSION
 
 [ -z $1 ] && abort "Provide an IMSCC file by dropping in this window or using: $0 /path/to/foo.imscc"
+
+echo "1: $1; INPUT: $INPUT_FILE"
 
 tar xvzf $INPUT_FILE -C $TEMPDIR > $TEMPDIR/extraction.log 2>&1
 
@@ -32,19 +36,19 @@ C_NAME=$(slugify "${NODES[0]}")
 
 echo "teacher: $T_NAME; course name: $C_NAME"
 
-OUTPUT_PATH=$OUTPUT_PATH/$T_NAME.$C_NAME
+FULL_PATH=$OUTPUT_PATH/$T_NAME.$C_NAME
 
-if [ ! -d "$OUTPUT_PATH" ] 
+if [ ! -d "$FULL_PATH" ] 
 then
-    mkdir "$OUTPUT_PATH"
+    mkdir "$FULL_PATH"
     if [ $? -ne 0 ]
     then
-        echo "could not create directory: $T_PATH"
+        echo "could not create directory: $FULL_PATH"
         echo "skipping"
     fi
 fi
 
 # find $TEMPDIR -type f -not -name "*.xml" -not -name "*.html" -exec mv {} $OUTPUT_PATH \;
-find $TEMPDIR -type f -not -name "*.xml" -not -name "*.html" | while read FILE ; do newfile="$(echo ${FILE} | sed  -E -e 's#(.*RES-[0-9a-zA-z]+/)([0-9a-zA-Z]+)(-)(.*)(\.[a-zA-Z0-9]{2,}$)#\4-\2\5#')" ; mv "${FILE}"  "${OUTPUT_PATH}/${newfile}" ; done
+find $TEMPDIR -type f -not -name "*.xml" -not -name "*.html" | while read FILE ; do newfile="$(echo ${FILE} | sed  -E -e 's#(.*RES-[0-9a-zA-z]+/)([0-9a-zA-Z]+)(-)(.*)(\.[a-zA-Z0-9]{2,}$)#\4-\2\5#')" ; mv "${FILE}"  "${FULL_PATH}/${newfile}" ; done
 
-echo "All regular files from $INPUT_FILE were extracted and placed in $OUTPUT_PATH. NOTE: no .html or .xml files were included."
+echo "All regular files from $INPUT_FILE were extracted and placed in $FULL_PATH. NOTE: no .html or .xml files were included."
